37d86edd042eccf37e4d63a4e4133eb9652c6d4c
Andrew Shaffer <andrew@weo.io>
2015-03-12 15:32:22 -0700

    initial

---
+++ index.js
@@ -0,0 +1,31 @@
+var ware = require('ware');
+var popup = require('oauth-popup');
+var qs = require('qs');
+
+module.exports = function(opts, getToken, cb) {
+  opts = opts || {};
+  if(! getToken)
+    throw new Error('must provide getToken function');
+
+  var mw = ware();
+  mw.use(popup(opts.popup || {}));
+  mw.use(getToken);
+  mw.use(opts.storeToken || storeToken());
+  mw.run(buildUrl(opts.provider), cb);
+}
+
+function buildUrl(provider) {
+  var url = provider.url;
+  return url + '?' + qs.stringify({
+    clientId: provider.clientId,
+    scopes: provider.scopes,
+    redirectUri: provider.redirectUri
+  });
+}
+
+function storeToken(name) {
+  return function(token, next) {
+    window.localStorage[name || 'oauth_token'] = token;
+    next();
+  };
+};
